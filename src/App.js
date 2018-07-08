ffbdb8d284002a639f3918df701092c3227b2559
Casper Kuijjer <casper@kuijjer.com>
2018-07-08 17:19:25 +0200

    some responsiveness ideas

--- src/App.js
+++ src/App.js
@@ -3,12 +3,25 @@ import React, { Component } from 'react'
 import Emoji from './Emoji'
 import emojis from './emojis.json'
 
+/*
+- responsiveness
+  - iPhone 320: 3 => minWidth: 100
+  - iPhone X 375: 3 => minWidth: 100
+  - iPhone Plus 414: 4 => minWidth: 100
+  === breakpoint (min-size: 768px)
+  - iPad portrait 768: 5 => minWidth 150
+  - iPad landscape 1024: 6 => minWidth: 150
+  === breakpoint (min-size: 1366)
+  - 1366: often used, 1024 up to 1440 should get 6
+  - 1440: 6 => minWidth: 240
+  - 1920: 8 => minWidth: 240
+*/
+
 class App extends Component {
   render() {
     const emojiStyle = {
-      minWidth: 200,
-      maxWidth: 300,
-      height: 200,
+      width: 235,
+      height: 235,
     }
 
     return (
@@ -18,7 +31,11 @@ class App extends Component {
           flexWrap: 'wrap',
         }}
       >
-        {emojis.map(emoji => <Emoji style={emojiStyle}>{emoji}</Emoji>)}
+        {emojis.map(emoji => (
+          <Emoji key={emoji} style={emojiStyle}>
+            {emoji}
+          </Emoji>
+        ))}
       </div>
     )
   }
